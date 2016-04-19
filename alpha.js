c2cc017d5e878b36737b5e756e5512b5176ca75e
Jessica Wynn <Jessicashinjo@gmail.com>
2016-04-19 11:05:10 -0500

    final working stack with spaces

--- alpha.js
+++ alpha.js
@@ -1,12 +1,17 @@
-var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+var alphabetLetters = ["no", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var stacker = [];
 
 function stackLetters(){
-  for (var i = 0; i < alphabetLetters.length; i++) {
-    stacker.push(alphabetLetters[i]);
-    console.log("alphabet stack", stacker);
+  for (var i = 1; i < alphabetLetters.length; i++) {
+    if(i % 5 === 0){
+      stacker.push(alphabetLetters[i], " ");
+      console.log("alphabet stack", stacker.join(""));
+    } else {
+      stacker.push(alphabetLetters[i]);
+      console.log("alphabet stack", stacker.join(""));
+    }
   }
 }
 
